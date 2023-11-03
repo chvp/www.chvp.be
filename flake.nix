08cffc2e6ae0217f485b937d182b530f9da17476
Charlotte Van Petegem <charlotte@vanpetegem.me>
2023-11-04 00:46:03 +0100

    Update flake dependencies

--- flake.nix
+++ flake.nix
@@ -2,16 +2,20 @@
   description = "My personal website";
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
     devshell = {
       url = "github:numtide/devshell";
       inputs = {
-        flake-utils.follows = "flake-utils";
         nixpkgs.follows = "nixpkgs";
+        systems.follows = "systems";
       };
     };
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+      inputs.systems.follows = "systems";
+    };
+    systems.url = "github:nix-systems/default";
   };
-  outputs = { self, nixpkgs, devshell, flake-utils }:
+  outputs = { self, nixpkgs, devshell, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem
       (system:
         let
